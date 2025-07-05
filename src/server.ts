7d3804d33540d42645fe180d8c133c1defeb8aad
Mattia Mandorlini <mando3228@gmail.com>
2025-07-05 19:05:25 +0200

    Test husky lint

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,6 @@
 import app from "./app";
 import config from "./config/config";
 
-const message = "hello world";
-
-function greet(name: string) {
-  console.log("Hi " + name);
-  console.log(message);
-}
-
-greet("Bro");
-
 app.listen(config.port, () => {
   console.log(`Server running on port ${config.port}`);
 });
