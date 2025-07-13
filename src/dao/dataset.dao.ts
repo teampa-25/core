585c4289d6fa0655a9d0e177e09297fb25b0ac48
Gabriele Vianello <vianello.tech@gmail.com>
2025-07-13 18:56:38 +0200

    fix: i need coffee

--- src/dao/dataset.dao.ts
+++ src/dao/dataset.dao.ts
@@ -1,107 +1,151 @@
 import { Dataset } from "@/models/dataset";
-import { IDAO } from "./idao";
+import { IDAO } from "@dao/interfaces/idao";
 import { ErrorEnum, getError } from "@/utils/api-error";
-import { Op } from "sequelize";
-
-export class DatasetDAO implements IDAO<Dataset>{
-    
-    async create(t: Dataset): Promise<string> {
-        try{
-            const createdDataset = await Dataset.create(t);
-            return createdDataset.id;
-        }catch(error){
-            throw(getError(ErrorEnum.GENERIC_ERROR))?.getErrorObj()
-        }
-    }
-
-    async filterByTags(tags: string[]): Promise<Dataset[]>{
-        try{
-            const datasets = await Dataset.findAll({
-                where: {
-                    tags: {
-                        [Op.overlap]: tags,
-                    },
-                },
-            });
-            return datasets;
-        }catch(error){
-            throw(getError(ErrorEnum.NOT_FOUND_ERROR))?.getErrorObj()
-        }
-    }
-
-    async get(id: string): Promise<Dataset | null> {
-        throw new Error("Method not implemented.");
-    }
-
-    async getAll(): Promise<Dataset[]> {
-        throw new Error("Method not implemented.");
-    }
-
-    async update(t: Dataset, ...params: any): Promise<boolean> {
-        throw new Error("Method not implemented.");
-    }
-
-    async delete(t: Dataset): Promise<boolean> {
-       try {
-        const id = t.id
-
-        const [updatedRowsCount] = await Dataset.update(
-            { deleted_at: new Date() },
-            { where: { id } }
-        );
-
-        return updatedRowsCount > 0;
-
-        } catch (error) {
-            throw(getError(ErrorEnum.NOT_FOUND_ERROR))?.getErrorObj()
-        }
-    }
-    
-}
-import { InferCreationAttributes } from "sequelize";
-import { IDAO } from "./interfaces/idao"
-import { Dataset } from "@/models/dataset";
-
-
+import { Op, InferCreationAttributes } from "sequelize";
 
 export class DatasetDAO implements IDAO<Dataset> {
+  async create(data: InferCreationAttributes<Dataset>): Promise<string> {
+    try {
+      const createdDataset = await Dataset.create(data);
+      return createdDataset.id;
+    } catch (error) {
+      throw getError(ErrorEnum.GENERIC_ERROR)?.getErrorObj();
+    }
+  }
+
+  async filterByTags(tags: string[]): Promise<Dataset[]> {
+    try {
+      const datasets = await Dataset.findAll({
+        where: {
+          tags: {
+            [Op.overlap]: tags,
+          },
+          deleted_at: null,
+        },
+      });
+      return datasets;
+    } catch (error) {
+      throw getError(ErrorEnum.NOT_FOUND_ERROR)?.getErrorObj();
+    }
+  }
 
   async get(id: string): Promise<Dataset | null> {
-    return Dataset.findByPk(id);
+    try {
+      const dataset = await Dataset.findOne({
+        where: {
+          id,
+          deleted_at: null,
+        },
+      });
+      return dataset;
+    } catch (error) {
+      throw getError(ErrorEnum.NOT_FOUND_ERROR)?.getErrorObj();
+    }
   }
 
   async getAll(): Promise<Dataset[]> {
-    return Dataset.findAll();
-  }
-
-  async update(id: string, data: Partial<Dataset>): Promise< Dataset | null> {
-    const update_dataset = await this.get(id);
-
-    if (!update_dataset) {
-      return null;
+    try {
+      const datasets = await Dataset.findAll({
+        where: {
+          deleted_at: null,
+        },
+      });
+      return datasets;
+    } catch (error) {
+      throw getError(ErrorEnum.NOT_FOUND_ERROR)?.getErrorObj();
     }
-
-    return update_dataset.update(data);
   }
 
-  //with paranoid mode enabled, the delete method will do a soft delete
+  async update(id: string, data: Partial<Dataset>): Promise<Dataset | null> {
+    try {
+      const dataset = await this.get(id);
+
+      if (!dataset) {
+        return null;
+      }
+
+      return await dataset.update(data);
+    } catch (error) {
+      throw getError(ErrorEnum.GENERIC_ERROR)?.getErrorObj();
+    }
+  }
+
   async delete(id: string): Promise<boolean> {
-    const delete_dataset = await this.get(id);
+    try {
+      const [updatedRowsCount] = await Dataset.update(
+        { deleted_at: new Date() },
+        {
+          where: {
+            id,
+            deleted_at: null,
+          },
+        },
+      );
 
-    if (!delete_dataset) {
-      return false;
+      return updatedRowsCount > 0;
+    } catch (error) {
+      throw getError(ErrorEnum.NOT_FOUND_ERROR)?.getErrorObj();
     }
-    await delete_dataset.destroy();
-    return true;
-  }
-
-  async create(data: InferCreationAttributes<Dataset>): Promise<string> {
-  const new_dataset = await Dataset.create(data);
-  
-  if (!new_dataset.id){
-      throw new Error("Dataset creation failed");
-    }
-    
-    return new_dataset.id; 
   }
 }
+
+// import { Dataset } from "@/models/dataset";
+// import { IDAO } from "@dao/interfaces/idao";
+// import { ErrorEnum, getError } from "@/utils/api-error";
+// import { Op } from "sequelize";
+
+// export class DatasetDAO implements IDAO<Dataset>{
+
+//     async create(t: Dataset): Promise<string> {
+//         try{
+//             const createdDataset = await Dataset.create(t);
+//             return createdDataset.id;
+//         }catch(error){
+//             throw(getError(ErrorEnum.GENERIC_ERROR))?.getErrorObj()
+//         }
+//     }
+
+//     async filterByTags(tags: string[]): Promise<Dataset[]>{
+//         try{
+//             const datasets = await Dataset.findAll({
+//                 where: {
+//                     tags: {
+//                         [Op.overlap]: tags,
+//                     },
+//                 },
+//             });
+//             return datasets;
+//         }catch(error){
+//             throw(getError(ErrorEnum.NOT_FOUND_ERROR))?.getErrorObj()
+//         }
+//     }
+
+//     async get(id: string): Promise<Dataset | null> {
+//         throw new Error("Method not implemented.");
+//     }
+
+//     async getAll(): Promise<Dataset[]> {
+//         throw new Error("Method not implemented.");
+//     }
+
+//     async update(t: Dataset, ...params: any): Promise<boolean> {
+//         throw new Error("Method not implemented.");
+//     }
+
+//     async delete(t: Dataset): Promise<boolean> {
+//        try {
+//         const id = t.id
+
+//         const [updatedRowsCount] = await Dataset.update(
+//             { deleted_at: new Date() },
+//             { where: { id } }
+//         );
+
+//         return updatedRowsCount > 0;
+
+//         } catch (error) {
+//             throw(getError(ErrorEnum.NOT_FOUND_ERROR))?.getErrorObj()
+//         }
+//     }
+
+// }
