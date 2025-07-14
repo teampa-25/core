febe7d67d65875b650608f8b7c854451da6e898e
Matteo04x <matteospiga2002@gmail.com>
2025-07-14 15:42:16 +0200

    fix: minor routes fixes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { UserController } from "@/controllers/user.controller";
+import { validate } from "@/middlewares/validate.middleware";
 import { authenticate } from "@/middlewares/authenticate.middleware";
 import { authorize } from "@/middlewares/authorize.middleware";
 import { UserRole } from "@/models/enums/user.role";
+import { UserSchema } from "@/utils/validation.schema";
 
 const router = Router();
 const userController = new UserController();
 
+router.post("/", validate(UserSchema.register), userController.register);
 router.use(authenticate, authorize(UserRole.USER, UserRole.ADMIN));
 router.get("/credits", userController.credits);
 
