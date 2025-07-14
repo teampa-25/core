4b5aaf41b0b1a3cc81b86e0f17a3b78d43e4c98f
Mattia Mandorlini <mando3228@gmail.com>
2025-07-14 09:19:47 +0200

    feat: catchAsync for controller

---
+++ src/utils/catchAsync.ts
@@ -0,0 +1,7 @@
+import type { NextFunction, Request, RequestHandler, Response } from "express";
+
+export function catchAsync(fn: RequestHandler): RequestHandler {
+  return (req: Request, res: Response, next: NextFunction) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
+  };
+}
