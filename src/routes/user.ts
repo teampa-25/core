0dbd952c337329596654c04bc74d329ce09a5b49
Mattia Mandorlini <mando3228@gmail.com>
2025-07-15 11:53:28 +0200

    fix: routes

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -3,13 +3,12 @@ import { UserController } from "@/controllers/user.controller";
 import { validate } from "@/middlewares/validate.middleware";
 import { authenticate } from "@/middlewares/authenticate.middleware";
 import { authorize } from "@/middlewares/authorize.middleware";
-import { UserRole } from "@/models/enums/user.role";
-import { UserSchema } from "@/utils/validation.schema";
+import { UserRole } from "@/common/enums";
+import { UserSchema } from "@/common/utils/validation-schema";
 
 const router = Router();
 const userController = new UserController();
 
-router.post("/", validate(UserSchema.register), userController.register);
 router.use(authenticate, authorize(UserRole.USER, UserRole.ADMIN));
 router.get("/credits", userController.credits);
 
